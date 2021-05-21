b78745369ff91dc6b4e7eaf375ecfbe2ceaabe14
waipy <liweisheng1234@qq.com>
2021-05-21 20:56:02 +0800

    add connection f1

--- connection.js
+++ connection.js
@@ -4,9 +4,12 @@ const sequelize = new Sequelize('hello', 'root', 'Bjfytw1234', {
   host: 'http://www.bjfytw.cn',
   dialect: 'mysql'
 });
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
+async function f1() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 }
+f1();

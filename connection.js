39d6d799cb304e95ec4c04aacec9db07d157c0f2
waipy <liweisheng1234@qq.com>
2021-05-21 20:52:46 +0800

    add connection test code

--- connection.js
+++ connection.js
@@ -1,1 +1,12 @@
 console.log('hello');
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('hello', 'root', 'Bjfytw1234', {
+  host: 'http://www.bjfytw.cn',
+  dialect: 'mysql'
+});
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
